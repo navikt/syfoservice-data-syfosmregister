e0635650d4d25328247173f202265f1ff259c248
Joakim Kartveit <joakimkartveit@gmail.com>
2019-10-29 15:11:48 +0100

    Dropper vault, får å få app til å kjøre

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -4,16 +4,11 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.util.KtorExperimentalAPI
-import java.nio.file.Paths
-import no.nav.syfo.aksessering.db.hentSykmeldinger
 import no.nav.syfo.application.ApplicationServer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.createApplicationEngine
-import no.nav.syfo.db.Database
-import no.nav.syfo.utils.getFileAsString
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -30,22 +25,21 @@ val log: Logger = LoggerFactory.getLogger("no.nav.syfo.syfoservicedatasyfosmregi
 fun main() {
     val environment = Environment()
 
-    val vaultSecrets = objectMapper.readValue<VaultCredentials>(Paths.get("/var/run/secrets/nais.io/vault/credentials/credentials.json").toFile())
+    /*val vaultSecrets = objectMapper.readValue<VaultCredentials>(Paths.get("/var/run/secrets/nais.io/vault/credentials/credentials.json").toFile())
 
     val vaultServiceuser = VaultServiceUser(
         serviceuserPassword = getFileAsString("/var/run/secrets/nais.io/vault/serviceuser/password"),
         serviceuserUsername = getFileAsString("/var/run/secrets/nais.io/vault/serviceuser/username")
     )
+    val database = Database(environment, vaultSecrets)
+     */
 
     val applicationState = ApplicationState()
-
-    val database = Database(environment, vaultSecrets)
-
     val applicationEngine = createApplicationEngine(environment, applicationState)
     val applicationServer = ApplicationServer(applicationEngine, applicationState)
 
     applicationServer.start()
     applicationState.ready = true
 
-    val hentsykmedlinger = database.hentSykmeldinger()
+    // val hentsykmedlinger = database.hentSykmeldinger()
 }
