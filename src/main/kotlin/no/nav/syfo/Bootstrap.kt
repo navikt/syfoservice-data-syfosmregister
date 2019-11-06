4cc80c68011e03a4ede0e6a1868e33cbbc8466ae
Joakim Kartveit <joakimkartveit@gmail.com>
2019-11-06 14:58:59 +0100

    Prøver å mappe fra kafka topic i dev

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -4,19 +4,25 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.util.KtorExperimentalAPI
+import java.io.StringReader
+import java.sql.Timestamp
+import java.time.Duration
+import java.time.LocalDateTime
+import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
 import no.nav.syfo.application.ApplicationServer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.createApplicationEngine
-import no.nav.syfo.db.Database
-import no.nav.syfo.kafka.SykmeldingKafkaProducer
 import no.nav.syfo.kafka.loadBaseConfig
-import no.nav.syfo.kafka.toProducerConfig
-import no.nav.syfo.service.SykmeldingService
-import no.nav.syfo.utils.JacksonKafkaSerializer
+import no.nav.syfo.kafka.toConsumerConfig
+import no.nav.syfo.model.ReceivedSykmelding
+import no.nav.syfo.model.toSykmelding
+import no.nav.syfo.utils.fellesformatUnmarshaller
 import no.nav.syfo.utils.getFileAsString
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -43,15 +49,21 @@ fun main() {
         serviceuserUsername = getFileAsString("/secrets/serviceuser/username")
     )
 
+    /*
     val vaultConfig = VaultConfig(
         jdbcUrl = getFileAsString("/secrets/config/jdbc_url")
-    )
+    )*/
 
     val kafkaBaseConfig = loadBaseConfig(environment, vaultServiceuser)
-    val producerProperties = kafkaBaseConfig.toProducerConfig(environment.applicationName, valueSerializer = JacksonKafkaSerializer::class)
-    val kafkaproducerStringSykmelding = KafkaProducer<String, String>(producerProperties)
+    val consumerProperties = kafkaBaseConfig.toConsumerConfig(
+        "${environment.applicationName}-consumer",
+        valueDeserializer = StringDeserializer::class
+    )
+    // val producerProperties = kafkaBaseConfig.toProducerConfig(environment.applicationName, valueSerializer = JacksonKafkaSerializer::class)
+    // val kafkaproducerStringSykmelding = KafkaProducer<String, String>(producerProperties)
+    val kafkaconsumerStringSykmelding = KafkaConsumer<String, String>(consumerProperties)
 
-    val database = Database(vaultConfig, vaultSecrets)
+    // val database = Database(vaultConfig, vaultSecrets)
 
     val applicationState = ApplicationState()
     val applicationEngine = createApplicationEngine(environment, applicationState)
@@ -59,6 +71,49 @@ fun main() {
 
     applicationServer.start()
     applicationState.ready = true
-    val sykmeldingKafkaProducer = SykmeldingKafkaProducer(environment.sm2013SyfoserviceSykmeldingTopic, kafkaproducerStringSykmelding)
-    SykmeldingService(sykmeldingKafkaProducer, database, 10_000).run()
+
+    kafkaconsumerStringSykmelding.subscribe(
+        listOf(environment.sm2013SyfoserviceSykmeldingTopic)
+    )
+
+    kafkaconsumerStringSykmelding.poll(Duration.ofMillis(0)).forEach { consumerRecord ->
+        val jsonMap: Map<String, Any?> = objectMapper.readValue(consumerRecord.value())
+        val receivedSykmelding = toReceivedSykmelding(jsonMap)
+        log.info("Mapped to ReceivedSykmelding")
+    }
+
+    // val sykmeldingKafkaProducer = SykmeldingKafkaProducer(environment.sm2013SyfoserviceSykmeldingTopic, kafkaproducerStringSykmelding)
+
+    // SykmeldingService(sykmeldingKafkaProducer, database, 10_000).run()
 }
+
+fun toReceivedSykmelding(jsonMap: Map<String, Any?>): ReceivedSykmelding {
+
+    val unmarshallerToHealthInformation = unmarshallerToHealthInformation(jsonMap["dokument"].toString())
+
+    return ReceivedSykmelding(
+        sykmelding = unmarshallerToHealthInformation.toSykmelding(
+            sykmeldingId = jsonMap["melding_id"].toString(),
+            pasientAktoerId = jsonMap["aktor_id"].toString(),
+            legeAktoerId = "",
+            msgId = "",
+            signaturDato = (jsonMap["created"] as Timestamp).toLocalDateTime()
+        ),
+        personNrPasient = unmarshallerToHealthInformation.pasient.fodselsnummer.id,
+        tlfPasient = unmarshallerToHealthInformation.pasient.kontaktInfo.firstOrNull()?.teleAddress?.v,
+        personNrLege = "",
+        navLogId = jsonMap["mottak_id"].toString(),
+        msgId = "",
+        legekontorOrgNr = "",
+        legekontorOrgName = "",
+        legekontorHerId = "",
+        legekontorReshId = "",
+        mottattDato = LocalDateTime.now(),
+        rulesetVersion = unmarshallerToHealthInformation.regelSettVersjon,
+        fellesformat = "",
+        tssid = ""
+    )
+}
+
+fun unmarshallerToHealthInformation(healthInformation: String): HelseOpplysningerArbeidsuforhet =
+    fellesformatUnmarshaller.unmarshal(StringReader(healthInformation)) as HelseOpplysningerArbeidsuforhet
--- src/main/kotlin/no/nav/syfo/aksessering/db/SyfoServiceQueries.kt
+++ src/main/kotlin/no/nav/syfo/aksessering/db/SyfoServiceQueries.kt
@@ -1,15 +1,9 @@
 package no.nav.syfo.aksessering.db
 
-import java.io.StringReader
 import java.sql.ResultSet
-import java.time.LocalDateTime
-import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
 import no.nav.syfo.db.DatabaseInterface
 import no.nav.syfo.db.toList
-import no.nav.syfo.model.ReceivedSykmelding
-import no.nav.syfo.model.toSykmelding
 import no.nav.syfo.objectMapper
-import no.nav.syfo.utils.fellesformatUnmarshaller
 
 data class DatabaseResult(val lastIndex: Int, val rows: List<String>, var databaseTime: Double = 0.0, var processingTime: Double = 0.0)
 
@@ -61,33 +55,6 @@ fun ResultSet.toJsonString(previusIndex: Int): DatabaseResult {
     }
     return DatabaseResult(lastIndex, listOfRows.map { objectMapper.writeValueAsString(it) })
 }
-
-fun ResultSet.toReceivedSykmelding(): ReceivedSykmelding =
-    ReceivedSykmelding(
-        sykmelding = unmarshallerToHealthInformation(getString("dokument")).toSykmelding(
-            sykmeldingId = getString("melding_id"),
-            pasientAktoerId = getString("aktor_id"),
-            legeAktoerId = "",
-            msgId = "",
-            signaturDato = getTimestamp("created").toLocalDateTime()
-        ),
-        personNrPasient = unmarshallerToHealthInformation(getString("dokument")).pasient.fodselsnummer.id,
-        tlfPasient = unmarshallerToHealthInformation(getString("dokument")).pasient.kontaktInfo.firstOrNull()?.teleAddress?.v,
-        personNrLege = "",
-        navLogId = getString("mottak_id"),
-        msgId = "",
-        legekontorOrgNr = "",
-        legekontorOrgName = "",
-        legekontorHerId = "",
-        legekontorReshId = "",
-        mottattDato = LocalDateTime.now(),
-        rulesetVersion = unmarshallerToHealthInformation(getString("dokument")).regelSettVersjon,
-        fellesformat = "",
-        tssid = ""
-    )
-
-fun unmarshallerToHealthInformation(healthInformation: String): HelseOpplysningerArbeidsuforhet =
-    fellesformatUnmarshaller.unmarshal(StringReader(healthInformation)) as HelseOpplysningerArbeidsuforhet
 
 fun DatabaseInterface.hentAntallSykmeldinger(): List<AntallSykmeldinger> =
     connection.use { connection ->
