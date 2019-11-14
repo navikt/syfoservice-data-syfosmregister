291325ba21f800951a91e5158e2317219317ac18
andreasnilsen <danduras@gmail.com>
2019-11-14 12:16:30 +0100

    log

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -13,7 +13,9 @@ import no.nav.syfo.db.DatabasePostgres
 import no.nav.syfo.db.VaultCredentialService
 import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toConsumerConfig
+import no.nav.syfo.model.Eia
 import no.nav.syfo.service.SkrivTilSyfosmRegisterServiceEia
+import no.nav.syfo.utils.JacksonKafkaSerializer
 import no.nav.syfo.utils.getFileAsString
 import no.nav.syfo.vault.RenewVaultService
 import org.apache.kafka.clients.consumer.ConsumerConfig
