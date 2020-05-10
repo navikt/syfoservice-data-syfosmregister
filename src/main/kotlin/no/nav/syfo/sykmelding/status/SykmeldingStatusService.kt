8d578ea204f9a249cdf445c2537a365c411cc8df
andreasnilsen <danduras@gmail.com>
2020-05-10 15:19:24 +0200

    handle slettet events

--- src/main/kotlin/no/nav/syfo/sykmelding/status/SykmeldingStatusService.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/status/SykmeldingStatusService.kt
@@ -17,6 +17,7 @@ import no.nav.syfo.getVaultServiceUser
 import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toProducerConfig
 import no.nav.syfo.log
+import no.nav.syfo.model.StatusEvent
 import no.nav.syfo.model.SykmeldingStatusEvent
 import no.nav.syfo.model.sykmeldingstatus.SykmeldingStatusKafkaMessageDTO
 import no.nav.syfo.persistering.db.postgres.getStatusesForSykmelding
@@ -66,7 +67,7 @@ class SykmeldingStatusService(
                     val newStatuses = statuses.map {
                         val mapped: SykmeldingStatusEvent
                         if (it.timestamp == null) {
-                            if (first) {
+                            if (first || it.event == StatusEvent.SLETTET) {
                                 mapped = it.copy(timestamp = it.eventTimestamp.atOffset(ZoneOffset.UTC))
                             } else {
                                 mapped = it.copy(timestamp = it.eventTimestamp.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC).toOffsetDateTime())
