53f4aa5312c9991ece6e3e3d5f1f5900071a561c
andreasnilsen <danduras@gmail.com>
2020-07-09 21:33:20 +0200

    More XML

--- src/main/kotlin/no/nav/syfo/legeerklaring/LegeerklaringMapper.kt
+++ src/main/kotlin/no/nav/syfo/legeerklaring/LegeerklaringMapper.kt
@@ -2,12 +2,28 @@ package no.nav.syfo.legeerklaring
 
 import com.fasterxml.jackson.module.kotlin.convertValue
 import com.fasterxml.jackson.module.kotlin.readValue
+import javax.xml.namespace.QName
 import no.nav.helse.eiFellesformat.XMLEIFellesformat
 import no.nav.helse.eiFellesformat.XMLMottakenhetBlokk
 import no.nav.helse.legeerklaering.Legeerklaring
+import no.nav.helse.msgHead.ObjectFactory
+import no.nav.helse.msgHead.XMLKeyValueType
 import no.nav.helse.msgHead.XMLMsgHead
+import no.nav.helse.msgHead.XMLRSAKeyValueType
+import no.nav.helse.msgHead.XMLX509DataType
 import no.nav.syfo.legeerklaringObjectMapper
+import no.nav.syfo.objectMapper
 import no.nav.syfo.utils.get
+
+data class CustomeX509Cert(val value: String)
+data class CustomeX509DataType(val x509IssuerSerialOrX509SKIOrX509SubjectName: List<CustomeX509Cert>)
+data class CustomX509JAXBElement(val name: QName, val declaredType: Class<Any>?, val value: CustomeX509DataType)
+
+data class CustomeKeyValueType(val modulus: String, val exponent: String)
+data class CustomKeyValueElement(val name: QName, val declaredType: Class<Any?>, val value: CustomeKeyValueType)
+data class CustomContent(var content: ArrayList<Any>)
+
+data class CustomXmlKeyValueElement(val name: QName, val declaredType: Class<Any>?, val value: CustomContent)
 
 class LegeerklaringMapper private constructor() {
     companion object {
@@ -20,12 +36,57 @@ class LegeerklaringMapper private constructor() {
             val xmlMsgHead = fellesformatJson.get<XMLMsgHead>()
             for (i: Int in xmlMsgHead.document.indices) {
                 if (xmlMsgHead.document[i].refDoc.msgType.v == "XML") {
-                    xmlMsgHead.document[i].refDoc.content.any[0] = legeerklaringObjectMapper.convertValue<Legeerklaring>(xmlMsgHead.document[i].refDoc.content.any[0])
+                    xmlMsgHead.document[i].refDoc.content.any[0] =
+                        legeerklaringObjectMapper.convertValue<Legeerklaring>(xmlMsgHead.document[i].refDoc.content.any[0])
+                }
+            }
+            if (xmlMsgHead.signature?.keyInfo?.content != null) {
+                xmlMsgHead.signature.keyInfo.content.removeIf { it is String }
+                for (i: Int in xmlMsgHead.signature.keyInfo.content.indices) {
+                    xmlMsgHead.signature.keyInfo.content[i] =
+                        convertKeyInfoToXmlobject(xmlMsgHead.signature.keyInfo.content[i])
                 }
             }
 
             xmlMsgHead.document.removeIf { it.documentConnection?.v == "V" }
             return fellesformatJson
+        }
+
+        private fun convertKeyInfoToXmlobject(any: Any): Any {
+            try {
+                val jsonNode = objectMapper.convertValue<CustomX509JAXBElement>(any)
+                if (jsonNode.declaredType == XMLX509DataType::class.java) {
+                    val x509Data = ObjectFactory().createXMLX509DataType()
+                    x509Data.x509IssuerSerialOrX509SKIOrX509SubjectName.add(
+                        ObjectFactory().createXMLX509DataTypeX509Certificate(
+                            jsonNode.value.x509IssuerSerialOrX509SKIOrX509SubjectName.get(0).value.toByteArray()
+                        )
+                    )
+                    return ObjectFactory().createX509Data(x509Data)
+                }
+            } catch (ex: Exception) {
+            }
+            try {
+                val jsonNode = objectMapper.convertValue<CustomXmlKeyValueElement>(any)
+                if (jsonNode.declaredType == XMLKeyValueType::class.java) {
+                    jsonNode.value.content.removeIf { it is String }
+                    val keyType = objectMapper.convertValue<CustomKeyValueElement>(jsonNode.value.content[0])
+                    if (keyType.declaredType == XMLRSAKeyValueType::class.java) {
+
+                        val valueType = ObjectFactory().createXMLRSAKeyValueType()
+                        valueType.exponent = keyType.value.exponent.toByteArray()
+                        valueType.modulus = keyType.value.modulus.toByteArray()
+                        ObjectFactory().createRSAKeyValue(valueType)
+                        val keyValueType = ObjectFactory().createXMLKeyValueType()
+                        val rsaKeyValueTYpe = ObjectFactory().createRSAKeyValue(valueType)
+                        keyValueType.content.add(rsaKeyValueTYpe)
+                        return ObjectFactory().createKeyValue(keyValueType)
+                    }
+                }
+            } catch (ex: Exception) {
+            }
+
+            throw RuntimeException("Error")
         }
 
         private fun convertToXmlobject(any: Any): Any {
