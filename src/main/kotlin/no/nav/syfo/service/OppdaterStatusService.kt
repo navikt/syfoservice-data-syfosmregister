6fb5b2b75049f72862df7b02d844d72cde1c1474
Tia Firing <tia.firing@nav.no>
2020-11-20 08:05:48 +0100

    oppdaterer status

--- src/main/kotlin/no/nav/syfo/service/OppdaterStatusService.kt
+++ src/main/kotlin/no/nav/syfo/service/OppdaterStatusService.kt
@@ -4,25 +4,37 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import no.nav.syfo.aksessering.db.oracle.settTilNy
 import no.nav.syfo.db.DatabaseOracle
+import no.nav.syfo.db.DatabasePostgres
 import no.nav.syfo.log
 import no.nav.syfo.model.sykmeldingstatus.STATUS_APEN
 import no.nav.syfo.model.sykmeldingstatus.SykmeldingStatusKafkaEventDTO
+import no.nav.syfo.persistering.db.postgres.hentSykmeldingMedId
 import no.nav.syfo.sykmelding.SykmeldingStatusKafkaProducer
 
-class OppdaterStatusService(private val databaseoracle: DatabaseOracle, private val sykmeldingStatusKafkaProducer: SykmeldingStatusKafkaProducer) {
+class OppdaterStatusService(
+    private val databaseoracle: DatabaseOracle,
+    private val sykmeldingStatusKafkaProducer: SykmeldingStatusKafkaProducer,
+    private val databasePostgres: DatabasePostgres
+) {
 
-    val sykmeldingId = "a9888b61-6da3-4e6e-a89a-21af5dfc5936"
+    val sykmeldingId = "121f00ee-f30f-4f19-9a67-38c6b1a97bd2"
 
-    fun start(fnr: String) {
-        val sykmeldingStatusKafkaEventDTO = SykmeldingStatusKafkaEventDTO(
-            sykmeldingId = sykmeldingId,
-            timestamp = OffsetDateTime.now(ZoneOffset.UTC),
-            statusEvent = STATUS_APEN,
-            arbeidsgiver = null,
-            sporsmals = null
-        )
-        sykmeldingStatusKafkaProducer.send(sykmeldingStatusKafkaEventDTO, "migrering", fnr)
-        log.info("Sendt statusendring")
-        databaseoracle.settTilNy(sykmeldingId)
+    fun start() {
+        val sykmelding = databasePostgres.connection.hentSykmeldingMedId(sykmeldingId)
+        if (sykmelding != null) {
+            log.info("oppdaterer status for sykmeldingid {}", sykmeldingId)
+            val sykmeldingStatusKafkaEventDTO = SykmeldingStatusKafkaEventDTO(
+                sykmeldingId = sykmeldingId,
+                timestamp = OffsetDateTime.now(ZoneOffset.UTC),
+                statusEvent = STATUS_APEN,
+                arbeidsgiver = null,
+                sporsmals = null
+            )
+            sykmeldingStatusKafkaProducer.send(sykmeldingStatusKafkaEventDTO, "migrering", sykmelding.sykmeldingsopplysninger.pasientFnr)
+            log.info("Sendt statusendring")
+            databaseoracle.settTilNy(sykmeldingId)
+        } else {
+            log.info("fant ikke sykmelding med id {}", sykmeldingId)
+        }
     }
 }
