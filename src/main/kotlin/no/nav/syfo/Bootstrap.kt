b5e3d8625f0208793140bbec1569e601647163bc
Tia Firing <tia.firing@nav.no>
2020-12-21 11:57:41 +0100

    retter periode

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -115,7 +115,7 @@ fun main() {
     applicationServer.start()
     applicationState.ready = true
 
-    // slettInfo(applicationState, environment)
+    updatePeriode(applicationState, environment)
 }
 
 fun getDatabasePostgres(): DatabasePostgres {
