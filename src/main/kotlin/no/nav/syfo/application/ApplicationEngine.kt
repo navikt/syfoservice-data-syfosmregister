170341584f86d485a04f40cae29bf0c166f283fc
Tia Firing <tia.firing@nav.no>
2021-01-05 16:20:24 +0100

    må kunne ta i mot json..

--- src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
@@ -1,7 +1,14 @@
 package no.nav.syfo.application
 
 import com.auth0.jwk.JwkProvider
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.application.install
 import io.ktor.auth.authenticate
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
 import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
@@ -22,6 +29,14 @@ fun createApplicationEngine(
     appIds: List<String>
 ): ApplicationEngine =
         embeddedServer(Netty, env.applicationPort) {
+            install(ContentNegotiation) {
+                jackson {
+                    registerKotlinModule()
+                    registerModule(JavaTimeModule())
+                    configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            }
             setupAuth(
                 jwkProviderInternal = jwkProviderInternal,
                 issuerServiceuser = issuerServiceuser,
