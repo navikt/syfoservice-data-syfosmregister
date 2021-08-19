7483f0209cd42e01eaea3d743f19735f702256ba
Tia Firing <tia.firing@nav.no>
2021-08-19 07:45:01 +0200

    patcher manuell-oppgaver

--- src/main/kotlin/no/nav/syfo/manuell/ValidationResultService.kt
+++ src/main/kotlin/no/nav/syfo/manuell/ValidationResultService.kt
@@ -2,6 +2,8 @@ package no.nav.syfo.manuell
 
 import no.nav.syfo.db.DatabasePostgresManuell
 import no.nav.syfo.log
+import no.nav.syfo.model.Status
+import no.nav.syfo.model.ValidationResult
 
 class ValidationResultService(
     private val databasePostgres: DatabasePostgresManuell
@@ -10,7 +12,7 @@ class ValidationResultService(
         val aktuelleOppgaver = databasePostgres.hentAktuelleManuellOppgaver()
         log.info("Fant ${aktuelleOppgaver.size} oppgaver som skal patches")
 
-        /*aktuelleOppgaver.forEach {
+        aktuelleOppgaver.forEach {
             log.info("Oppdaterer oppgave med id ${it.oppgaveid}")
             val oppdatertOppgave = ManuellOppgave(
                 validationResult = ValidationResult(Status.OK, emptyList()),
@@ -18,7 +20,7 @@ class ValidationResultService(
                 opprinneligValidationResult = it.validationResult
             )
             databasePostgres.oppdaterManuellOppgave(oppdatertOppgave)
-        }*/
+        }
         log.info("Ferdig med patching")
     }
 }
